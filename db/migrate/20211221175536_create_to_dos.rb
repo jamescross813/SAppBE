f723072cd310810022a6a1e2e4dbb945f3e28210
jamescross813 <crossjames813@gmail.com>
2021-12-21 12:06:52 -0600

    table update

--- db/migrate/20211221175536_create_to_dos.rb
+++ db/migrate/20211221175536_create_to_dos.rb
@@ -1,8 +1,8 @@
 class CreateToDos < ActiveRecord::Migration[6.1]
   def change
     create_table :to_dos do |t|
-      t.string :todo
-      t.string :dodate
+      t.string :to_do
+      t.string :do_date
       t.belongs_to :project, null: false, foreign_key: true
 
       t.timestamps
