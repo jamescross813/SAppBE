8df492457eea8d19e4625877a48d72524a36b5eb
jamescross813 <crossjames813@gmail.com>
2021-12-21 12:06:26 -0600

    table update

--- db/migrate/20211221175403_create_projects.rb
+++ db/migrate/20211221175403_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
     create_table :projects do |t|
       t.string :title
       t.string :due_date
+      t.text :read_me
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
