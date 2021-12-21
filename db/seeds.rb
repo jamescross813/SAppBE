1909daefc76565495e317c95876fc28bbcbca5f9
jamescross813 <crossjames813@gmail.com>
2021-12-21 12:14:32 -0600

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "James", password: "1234")
+project = Project.create(title: "ToDo app", due_date: "12/23/2021", read_me: "An app to track what projects you're working on and keep track of tasks still do to/add new tasks and notes when away from the computer", user: User.find_by(id: 1))
