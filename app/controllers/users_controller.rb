c5d87f7284e0e57c0c9a1a520fcf9ff34c5c2aa6
jamescross813 <crossjames813@gmail.com>
2021-12-22 09:00:44 -0600

    create method complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(user_params)
-        
+        if user.save
+            render json: user,
+            except: [:create_at, :updated_at]
+        else
+            render json: {message: "Could not create user, try again!"}
+        end
     end
 
     private
