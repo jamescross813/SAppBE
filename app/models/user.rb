ce98c3439fd7737a49a4d90e12ebf54826b9eb54
jamescross813 <crossjames813@gmail.com>
2021-12-22 08:29:15 -0600

    relationship typo fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :projects
     # has_many :notes, through: :projects
-    # has_many :todos, through: :projects
+    # has_many :to_dos, through: :projects
 end
