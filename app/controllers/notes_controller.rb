21ee200f7ad740123dbadaccdbbf3fefee38acae
jamescross813 <crossjames813@gmail.com>
2021-12-24 14:00:48 -0600

    complete create action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,12 @@
 class NotesController < ApplicationController
     def create
+        note = Note.new(notes_params)
+        if note.save
+            render json: note,
+            except: [created_at, :updated_at]
+        else
+            render json: {message: "Could not create a note, try again"}
+        end
     end
 
     private
