5430d84d5ceeaa2dcaaf78c9749aefc495f60c44
jamescross813 <crossjames813@gmail.com>
2021-12-24 12:55:34 -0600

    create project basic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,12 @@
 class ProjectsController < ApplicationController
+
+    def create
+        project = Project.new(project_params)
+    end
+
+    private
+    
+    def project_params
+        params.require(:project).permit(:title, :user_id)
+    end
 end
