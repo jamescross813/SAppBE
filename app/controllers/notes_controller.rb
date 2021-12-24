a7d76d503be0d7cdd849fb519ef624e0c3ac0777
jamescross813 <crossjames813@gmail.com>
2021-12-24 14:05:28 -0600

    destory action added

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -36,6 +36,11 @@ class NotesController < ApplicationController
         end
     end
 
+    def destroy
+        note = Note.find_by(id: params[:id])
+        note.destroy
+    end
+
     private
 
     def notes_params
