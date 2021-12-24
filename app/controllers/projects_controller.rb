341cd72665cce8cb306b26e5c8238c4d76547f0d
jamescross813 <crossjames813@gmail.com>
2021-12-24 12:57:01 -0600

    new project save and check

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,12 @@ class ProjectsController < ApplicationController
 
     def create
         project = Project.new(project_params)
+        if project.save
+            render json: project,
+            except: [:create_at, :updated_at]
+        else
+            render json:{message: "Could not create project, try again please!"}
+        end
     end
 
     private
