e279d4b5fcf2bd7e7d3236a74b8b613dee97a72d
jamescross813 <crossjames813@gmail.com>
2021-12-24 13:56:50 -0600

    strong params included, start of create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,10 @@
 class NotesController < ApplicationController
+    def create
+    end
+
+    private
+
+    def notes_params
+        params.require(:note).permit(:note_content, :project_id)
+    end
 end
